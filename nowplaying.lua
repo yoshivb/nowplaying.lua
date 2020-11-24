15b2074421e78753a5dac778141366b161d5a19f
Yoshi van Belkom <yoshivb@users.noreply.github.com>
2020-11-24 22:01:52 +0100

    Updated nowplaying.lua
    Updated the extension description.
    Added a config variable.
    Added support for customizing the output.
    Added support for outputting the album art.

--- nowplaying.lua
+++ nowplaying.lua
@@ -1,4 +1,4 @@
--- This version was tested using VLC version 2.2.1 --
+-- This version was tested using VLC version 3.0.11 (Windows) --
 
 --[[
 INSTALLATION:
@@ -6,27 +6,48 @@ Put the file in the VLC subdir /lua/extensions, by default:
 * Windows (all users): C:\Program Files (x86)\VideoLAN\VLC\lua\extensions
 * Windows (current user): C:\Users\USER\AppData\Roaming\vlc\lua\extensions
 To load extension simply restart VLC or from the menu bar Tools -> Plugins and extensions -> Active extensions click Reload extensions
-Activate the extension by going to the "View" menu and click on "Now Playing Radio: Write song name and artist to file."
+Activate the extension by going to the "View" menu and click on "Now Playing Info"
+
+CONFIG:
+In the config variable below you can setup the pattern for converting to the metadata into your now_playing.txt content.
+Some common meta keys:
+description,
+date,
+artwork_url,
+genre,
+track_total,
+album,
+track_number,
+filename,
+publisher,
+copyright,
+artist,
+language,
+title
 --]]
 
 -- Loads required libraries
 require "io"
 require "string"
 
+local config = {
+    infoPattern = "{artist} - {album} - {title}"
+}
+
 -- VLC Extension Descriptor
 function descriptor()
 	return {
-				title = "Now Playing Radio";
-				version = "0.1";
-				author = "J-Witz";
-				url = 'https://github.com/j-witz';
-				shortdesc = "Now Playing Radio: Write song name and artist to file.";
-				description = "Writes the current song name and artist to a text file.<br />"
-							.."(Based on the script made by Richard Andrew Cattermole)<br />"
-							.."(https://gist.github.com/rikkimax)<br />"
-							.."(Modified by J-Witz)";
-				capabilities = { "meta-listener" };
-			}
+        title = "Now Playing Info";
+        version = "0.9";
+        author = "yoshivb";
+        url = 'https://github.com/yoshivb';
+        shortdesc = "Now Playing Info";
+        description = "Writes the current song info and album art into files.<br />"
+                    .."(Fork from j-witz's extension)<br />"
+                    .."(https://github.com/J-Witz/nowplaying.lua)<br />"
+                    .."(Modified by yoshivb)";
+        capabilities = { "meta-listener" };
+    }
 end
 
 -- This function is triggered when the extension is activated
@@ -47,24 +68,41 @@ function meta_changed()
 	handleItem()
 end
 
--- Reads artist and title information and writes them to the text file
+-- Reads meta information and writes them to the files
 function handleItem()
 	local item = vlc.item or vlc.input.item()
-	local artistFile=vlc.config.homedir() .. "/now_playing_artist.txt"
-	local titleFile=vlc.config.homedir() .. "/now_playing_title.txt"
-	local artistFile2 = io.open(artistFile, "w+")
-	local titleFile2 = io.open(titleFile, "w+")
-	if not (item == nil) and vlc.input.is_playing() then  
+	if not (item == nil) and vlc.input.is_playing() then   
+        local infoFilePath = vlc.config.homedir() .. "/now_playing.txt"
+        local artFilePath = vlc.config.homedir() .. "/now_playing.jpg"
+        local infoFile = io.open(infoFilePath, "w+")
+    
+        local infoString = config.infoPattern
+        
 		local metas = item:metas()
-		local artist, title = string.match(metas.now_playing,'^%s*(.-)%s*%-%s*(.-)%s*$')
-		local artistText = "Artist: "..artist
-		local titleText = "Title: "..title
-		artistFile2:write(artistText)
-		titleFile2:write(titleText)
-	else
-		artistFile2:write("No song currently playing")
-		titleFile2:write("No song currently playing")
+        if metas then
+            for key, value in pairs(metas) do
+                infoString = string.gsub(infoString, "{" .. key .. "}", value)
+            end
+        else
+            vlc.msg.info("no meta data available")
+        end
+        infoString = string.gsub(infoString, "{.}", "")
+               
+        infoFile:write(infoString)
+        
+        if not (metas.artwork_url == nil) then
+            local artwork_path = vlc.strings.make_path(metas.artwork_url)
+            local artInFile = io.open(artwork_path, "rb")
+            local artBytes = artInFile:read("*a")
+            artInFile:close()
+
+            local artFile = io.open(artFilePath, "wb")
+            artFile:write(artBytes)
+            artFile:close()
+        else
+            vlc.msg.info("no artwork data available")
+        end
+        
+        infoFile:close()
 	end
-	artistFile2:close()
-	titleFile2:close()
 end
